8756c240fa9a2cff5358bdac8f7e3d5acecf6ca5
kylie <kcave@smith.edu>
2025-02-18 13:01:05 -0500

    Day 1 Work

--- Car.java
+++ Car.java
@@ -1,3 +1,6 @@
 public class Car {
+//make sure you check that there are seats remaining and that the passenger is not already on that car
+//check to make sure that a passenger is on that car before removing them 
+//add passenger and remove passenger should be public 
 
 }
--- Passenger.java
+++ Passenger.java
@@ -1,5 +1,6 @@
 public class Passenger {
-    
+    //make sure you don't check add capacity twice the car method already does it
+    //should use add passenger and remove passenger methods from car 
     private String name;
 
     public Passenger(String name) {
